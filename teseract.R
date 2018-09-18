0cc7201364c86865cb64f1bfb8c4f000f51fb1cb
Xingwei Ji <jxwjj6@gmail.com>
2018-09-18 10:27:58 -0700

    Add files via upload

---
+++ teseract.R
@@ -0,0 +1,21 @@
+library(tesseract)
+library(magick)
+library(tidyr)
+set.seed(100)
+eng <- tesseract("eng")
+text <- ocr_data("~/Desktop/DataFest/wine_test.jpg",engine = eng)
+# input <- image_read("~/Desktop/DataFest/wine_test.jpg")
+# text <- input %>%
+#   image_trim(fuzz = 40) %>%
+#   image_write(format = 'png', density = '300x300') %>%
+#   tesseract::ocr_data()
+filtered = text[which(text$confidence > 70),] #& text$word!="." & text$word != "-" & text$word != "--"),]
+filtered1 = separate(filtered, bbox, into = c("xstart","ystart","xend","yend"))
+filtered1$xstart = as.numeric(filtered1$xstart)
+filtered1$xend = as.numeric(filtered1$xend)
+filtered1$ystart = as.numeric(filtered1$ystart)
+filtered1$yend = as.numeric(filtered1$yend)
+filtered_name = filtered1[which(filtered1$xstart >= 466 & filtered1$xend <= 1732),]
+
+#str(filtered_name)
+
