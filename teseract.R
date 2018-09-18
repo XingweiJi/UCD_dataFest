c810e939b819df5cf7677d2413f4e937dcb6bfc4
Xingwei Ji <jxwjj6@gmail.com>
2018-09-18 12:09:27 -0700

    Update teseract.R

--- teseract.R
+++ teseract.R
@@ -9,7 +9,7 @@ text <- ocr_data("~/Desktop/DataFest/wine_test.jpg",engine = eng)
 #   image_trim(fuzz = 40) %>%
 #   image_write(format = 'png', density = '300x300') %>%
 #   tesseract::ocr_data()
-filtered = text[which(text$confidence > 70),] #& text$word!="." & text$word != "-" & text$word != "--"),]
+filtered = text[which(text$confidence > 70 & text$word!="." & text$word != "-" & text$word != "--"),]
 filtered1 = separate(filtered, bbox, into = c("xstart","ystart","xend","yend"))
 filtered1$xstart = as.numeric(filtered1$xstart)
 filtered1$xend = as.numeric(filtered1$xend)
@@ -18,4 +18,11 @@ filtered1$yend = as.numeric(filtered1$yend)
 filtered_name = filtered1[which(filtered1$xstart >= 466 & filtered1$xend <= 1732),]
 
 #str(filtered_name)
-
+cut(filtered_name$ystart,60)
+hist(filtered_name$ystart)
+arr = c()
+for(i in 2:116)
+{
+  if((filtered_name$ystart[i+1] - filtered_name$ystart[i]) > 60)
+    arr = c(arr,i)
+}
